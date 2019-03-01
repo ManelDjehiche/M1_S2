ebef330fd2b93ba5398b887443ce1da1f6d7cf2c
Manel Djehiche <43585421+ManelDjehiche@users.noreply.github.com>
2019-03-01 22:41:34 +0100

    Add files via upload

---
+++ tp1.java
@@ -0,0 +1,23 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class tp1 {
+    
+    public static void main(String[] args) throws FileNotFoundException{
+    
+             File temp =new File("uf20-01.cnf");
+             Scanner file= new Scanner(temp);
+    }
+}
